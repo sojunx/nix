051d1126144c80b2241410594dd6484a3418fc4e
Phat Le <phatlee1104@gmail.com>
2026-03-18 07:20:49 +0700

    update hyprland config

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -5,12 +5,22 @@
   environment.systemPackages = with pkgs; [ 
     kitty # Terminal 
     waybar # Status bar
-    rofi # Application launcher
+    wofi # Application launcher
     nautilus # File manager
+    wl-clipboard # Copy / Paste
   ];
   
   # Enable Hyprland
-  programs.hyprland.enable = true;  
-  programs.hyprland.xwayland.enable = true;
+  programs.hyprland = { 
+    enable = true;  
+    xwayland.enable = true;
+    withUWSM = true;
+  };
 
+  # Enabel XDG Portal
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+
+  # Enable Policy Kit
+  security.polkit.enable = true;
 }
